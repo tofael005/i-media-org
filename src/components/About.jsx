720c4d9398db0261e575cab24217debe242a17d5
Arnob <arnobtuhin.j@gamil.com>
2023-08-31 22:29:23 +0600

    about fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,12 @@ const About = () => {
         <div className="mt-24">
             <div className="max-w-[1240px] mx-auto">
                 <div className="grid md:grid-cols-2 gap-3 px-3">
-                    <Image className="rounded-md" src={about} alt=''></Image>
+                    <Image className="rounded-md md:w-full object-cover md:h-[520px]" src={about} alt=''></Image>
                     <div className="mt-8">
                         <h1 className="font-bold text-[#0B8998]">About Us</h1>
-                        <h1 className="text-4xl font-bold leading-snug my-3 text-[#23306C]">Welcome to Almanar Islamic Center for Muslims</h1>
-                        <p className="mb-6 font-light">slamic center” is a place for the Muslim community to refresh their knowledge and non-muslims to know about Islam. It creates awareness about the Islamic lifestyle and also the teachings of the prophet Muhammad(s.a.w). The Hajj house inside the Islamic center will be more helpful and guidance for them to perform Umrah and Hajj.</p>
-                        <p className="font-light">Almanar Islamic Center depends on your continuous support and generosity. Donate towards one of our featured campaigns or choose a campaign</p>
+                        <h1 className="text-4xl font-bold leading-snug my-3 text-[#23306C]">Welcome to Islamic Media Organization Center for Muslims</h1>
+                        <p className="mb-6 font-light text-justify">Islamic Media Organization Center” is a place for the Muslim community to refresh their knowledge and non-muslims to know about Islam. It creates awareness about the Islamic lifestyle and also the teachings of the prophet Muhammad(s.a.w). The Hajj house inside the Islamic center will be more helpful and guidance for them to perform Umrah and Hajj.</p>
+                        <p className="font-light text-justify">Islamic Media Organizatin Center depends on your continuous support and generosity. Donate towards one of our featured campaigns or choose a campaign</p>
                         <Link href="/about">
                             <button className="mt-10 rounded-md hover:bg-transparent hover:border-2 hover:border-bg-[#06DAD7] text-white font-bold hover:text-[#23306C] duration-500 bg-[#06DAD7] px-8 py-4">About More</button>
                         </Link>
--- src/components/PrayerTime.jsx
+++ src/components/PrayerTime.jsx
@@ -8,50 +8,50 @@ const PrayerTime = () => {
             <h1 className="text-center text-4xl text-[#23306C] font-bold">Prayar Times</h1>
             <p className="text-center mt-2 text-[#0B8998]">---Today---</p>
 
-            <div className="grid md:grid-cols-5 mt-8 gap-6">
-                <div className="border rounded-md flex items-center gap-3 p-4">
+            <div className="grid md:grid-cols-5 mt-8 gap-6 px-3">
+                <div className="border rounded-md md:flex flex justify-center items-center gap-3 p-4">
                     <span className="text-5xl text-center text-[#23306C] font-extrabold">
                         <BsStopwatch />
                     </span>
                     <div className="p-3">
-                        <h1 className="font-bold text-[#0B8998] mb-2">Fajar</h1>
-                        <p>5:10AM</p>
+                        <h1 className="font-bold text-[#0B8998] mb-2">FAZR</h1>
+                        <p>4:22AM</p>
                     </div>
                 </div>
-                <div className="border rounded-md flex items-center gap-3 p-4">
+                <div className="border rounded-md md:flex flex justify-center items-center gap-3 p-4">
                     <span className="text-5xl text-[#23306C] text-center font-extrabold">
                         <BsStopwatch />
                     </span>
                     <div className="p-3">
-                        <h1 className="font-bold text-[#0B8998] mb-2">Zuhr</h1>
-                        <p>12: 25PM</p>
+                        <h1 className="font-bold text-[#0B8998] mb-2">DHUHR</h1>
+                        <p>11: 59AM</p>
                     </div>
                 </div>
-                <div className="border rounded-md flex items-center gap-3 p-4">
+                <div className="border rounded-md md:flex flex justify-center items-center gap-3 p-4">
                     <span className="text-5xl text-[#23306C] text-center font-extrabold">
                         <BsStopwatch />
                     </span>
                     <div className="p-3">
-                        <h1 className="font-bold text-[#0B8998] mb-2">Asr</h1>
-                        <p>4: 35PM</p>
+                        <h1 className="font-bold text-[#0B8998] mb-2">ASR</h1>
+                        <p>4: 30PM</p>
                     </div>
                 </div>
-                <div className="border rounded-md flex items-center gap-3 p-4">
+                <div className="border rounded-md md:flex flex justify-center items-center gap-3 p-4">
                     <span className="text-5xl text-[#23306C] text-center font-extrabold">
                         <BsStopwatch />
                     </span>
                     <div className="p-3">
-                        <h1 className="font-bold text-[#0B8998] mb-2">Maghrib</h1>
-                        <p>5:10AM</p>
+                        <h1 className="font-bold text-[#0B8998] mb-2">MAGRIB</h1>
+                        <p>6:18PM</p>
                     </div>
                 </div>
-                <div className="border rounded-md flex items-center gap-3 p-4">
+                <div className="border rounded-md md:flex flex justify-center items-center gap-3 p-4">
                     <span className="text-5xl text-[#23306C] text-center font-extrabold">
                         <BsStopwatch />
                     </span>
                     <div className="p-3">
-                        <h1 className="font-bold text-[#0B8998] mb-2">Ishaa</h1>
-                        <p>5:10AM</p>
+                        <h1 className="font-bold text-[#0B8998] mb-2">ISHAA</h1>
+                        <p>7:35PM</p>
                     </div>
                 </div>
             </div>
