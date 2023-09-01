97a18ef8d7f7ef9eb0d2d960300c17a358180b86
Arnob <arnobtuhin.j@gamil.com>
2023-09-02 02:16:24 +0600

    added babys

--- src/app/golpo4/page.jsx
+++ src/app/golpo4/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
     return (
         <div className='max-w-[1240px] mx-auto mt-12 text-center'>
             <h1>Hello Dear</h1>
-            <p className='text-4xl my-5'>Golpo Four Coming SOON</p>
+            <p className='text-4xl my-5'>Golpo Four Coming SOOON</p>
         </div>
     );
 };
